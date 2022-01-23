72d25979dfc055a03dd0cf3e4ad9d56642f69ef5
Jingu Kang <jinguk0816@gmail.com>
2022-01-23 12:13:21 -0500

    add margin to the error message componnent

--- src/view/Error.jsx
+++ src/view/Error.jsx
@@ -3,8 +3,8 @@ import useWeather from "../controller/hook";
 
 export default function Error () {
   const {error} = useWeather();
-  
+
   return (
-    <Typography>{error && error.includes(404) ? "Oops!, The city does not exist. Please input correct city name!" : "Server Error, Please try again!"}</Typography>
+    <Typography sx={{ marginTop: "2em" }}>{error && error.includes(404) ? "Oops!, The city does not exist. Please input correct city name!" : "Server Error, Please try again!"}</Typography>
   );
 }
