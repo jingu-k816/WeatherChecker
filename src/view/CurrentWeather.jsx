5b053c794a4a6be5ca95bc4589a00c4e375f52b2
Jingu Kang <jinguk0816@gmail.com>
2022-01-23 01:53:05 -0500

    add more details to the current weather condition and put styles to it

--- src/view/CurrentWeather.jsx
+++ src/view/CurrentWeather.jsx
@@ -1,17 +1,31 @@
 import { Card, CardContent, Typography } from "@mui/material";
+import Image from "next/image";
 import useWeather from "../controller/hook";
 
 export default function CurrentWeather () {
   const {weatherData} = useWeather();
+  const iconURL = `http://openweathermap.org/img/wn/${weatherData && weatherData.weather[0].icon}.png`
 
   console.log("weatherdata in current weather component: ", weatherData && weatherData);
 
   return (
-    <Card sx={{ width: "15em", backgroundColor: "rgba(105, 105, 105, 0.4)", height: "20em"}}>
+    <Card sx={{ marginTop: "2em", width: "15em", backgroundColor: "rgba(105, 105, 105, 0.4)", height: "17em", textAlign: "center"}}>
       { weatherData && 
-        <CardContent>
-            <Typography variant="h5">{weatherData.name}</Typography>
-            
+        <CardContent >
+          <Typography variant="h5">{weatherData.name}</Typography>
+          <Image 
+            src={iconURL} 
+            alt="Today's weather icon"
+            width={100}
+            height={100}
+          />
+          <div>
+            <Typography sx={{ fontSize: "0.8rem"}}>Maximum Temperature: {weatherData.main.temp_max} &#8451;</Typography>
+            <Typography sx={{ fontSize: "0.8rem"}}>Minimum Temperature: {weatherData.main.temp_min} &#8451;</Typography>
+            <Typography sx={{ fontSize: "0.8rem"}}>Current Temperature: {weatherData.main.temp} &#8451;</Typography>
+            <Typography sx={{ fontSize: "0.8rem"}}>Feels like: {weatherData.main.feels_like} &#8451;</Typography>
+            <Typography sx={{ fontSize: "0.8rem"}}>Humidity: {weatherData.main.humidity}%</Typography>
+          </div>
         </CardContent>
       }
     </Card>
