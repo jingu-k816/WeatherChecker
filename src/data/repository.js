0ab3fa3ea8c71ff69c82098615fd5eb643290811
Jingu Kang <jinguk0816@gmail.com>
2022-01-23 01:25:00 -0500

    add metric units for forecast

--- src/data/repository.js
+++ src/data/repository.js
@@ -5,7 +5,7 @@ const APIKEY = "35751ae25bf6569a6b973f0691488ad0";
 const weatherRepository = {
   retrieveWeatherForecast: async (city) => {
     try {
-      const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKEY}`).then(res => {
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${APIKEY}`).then(res => {
         return res;
       })
       return response;
@@ -16,7 +16,6 @@ const weatherRepository = {
   retrieveCurrentWeather: async (city) => {
     try {
       const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKEY}`).then(res => {
-        console.log(res);
         return res;
       })
       return response;
